a424075cffbc4d62d64c945ffee2f40cba830e6a
AJtheManager <ayomidejoe48@gmail.com>
2026-03-30 11:32:56 +0100

    Remove backend/target from git tracking and update .gitignore

--- backend/target/debug/build/utoipa-swagger-ui-8d305099f7ad84a6/out/embed.rs
+++
@@ -1,5 +0,0 @@
-
-// This file is auto-generated during compilation, do not modify
-#[derive(RustEmbed)]
-#[folder = r"/home/olowo/Desktop/Ayomide /ttt/ChainLogistics/backend/target/debug/build/utoipa-swagger-ui-8d305099f7ad84a6/out/swagger-ui-5.3.1/dist/"]
-struct SwaggerUiDist;
